9435a71e4ce0e02a0831efcb0bdfdb79a26fcc88
xiaoma <1223841837@qq.com>
2023-12-07 21:46:13 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/users/:id', to: 'users#show'
-  post '/users/', to: 'users#create'
+  namespace :api do
+    namespace :v1 do
+      resources :validation_codes, only: [:create]
+      resources :session, only: [:create,:destroy]
+      resources :me, only: [:show]
+      resources :items 
+      resources :tags
 
+    end
+  end
 end
